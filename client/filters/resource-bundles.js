3264fdc28fb9be3751e10460fe79e19c168e4d09
amirpais <amirpais1@gmail.com>
2015-10-10 20:49:45 +0300

    add resource filter js

---
+++ client/filters/resource-bundles.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Pais on 10/10/2015.
+ */
+
+ var lang = 'HE'
+ var languages = {
+     'HE': 'he',
+     'EN': 'en'
+ };
+ var RESOURCE = {
+     he: {
+         //global
+         'global.firstName': 'שם פרטי',
+         'global.lastName': 'שם משפחה',
+     },
+     en: {
+     }
+ };
+angular.module('poalim-home').filter('trans', ['$scope', '$meteor',function ($scope, $meteor) {
+                                                return function (input) {
+                                                    return RESOURCE[languages[lang]][input];
+                                                };
+                                   }]);
+
+
